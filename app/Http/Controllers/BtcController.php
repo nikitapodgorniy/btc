a11e2a321ff398e2715a27e479e5df8155d247e9
nikitapodgorniy <32936126+nikitapodgorniy@users.noreply.github.com>
2021-10-13 20:24:47 +0300

    commit

--- app/Http/Controllers/BtcController.php
+++ app/Http/Controllers/BtcController.php
@@ -9,13 +9,11 @@ class BtcController extends Controller
 {
     public static function update(){
 
-        // $tasks = App\Models\Btc::create();
         $value = App\Models\Btc::get_api();
 
         $Btc = App\Models\Btc::find(1);
-
+        //добавить если null, то создание
         $Btc->value = $value;
-
         $Btc->save();
 
         return $Btc->value;
--- routes/web.php
+++ routes/web.php
@@ -16,84 +16,30 @@ use GuzzleHttp\Client;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('btc2',[BtcController::class, 'update']);
 
 Route::get('/btc', function () {
-    //$tasks = DB::table('tasks')->get();
-    //$tasks = App\Models\Task::all();
-    //$tasks = App\Models\Task::incomplete();
-    //$btc['name'] = 'btc';
-    //$btc['value'] = App\Models\Btc::get_api();
     $value = App\Models\Btc::get_api();
-    //App\Models\Btc::create($btc);
-    
-    //DB::table('btcs')->insert( ['name' => 'btc', 'value' => $value] );
-    
-    //dd($value);
-    //return view('tasks.index', compact('btc'));
-
-
-    //$res = DB::table('btcs')->where('name', 'btc')->get();
-
-    // DB::table('btcs')->where('name', 'btc')->update(array(
-    //     'value'=>$value,
-    // ));
-
-    $data['name'] = 'btc';
-    $data['value'] = $value;
-
-    //$value = App\Models\Btc::set($value);
 
     $Btc = App\Models\Btc::find(1);
-
     $Btc->value = $value;
-
     $Btc->save();
 
-    //dd($value);
-
     $res = DB::table('btcs')->where('name', 'btc')->first();
-
-    
-
 
     dd($res->value);
     
 });
 
 Route::get('btc1', function () {
-    /*
-    $price = DB::table('users')->get();
-    
-    return dd($price);
-    */
     $client = new Client();
-
-    //$client->setSslVerification(false);
-
-    //$client->getClient()->setDefaultOption('config/curl/' . CURLOPT_SSL_VERIFYPEER, false);
-
-     $url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
-    //$url = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR';
-    //$api_key = '&api_key=18b9e066bdd4ab3edf4d4c6cf4cc7d0ded538d0a99ac785571cfdc27da5e32ac';
-
-    //$url .= $api_key;
-
+    $url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
     $response = $client->request('GET', $url);
-    
-    
     $res = json_decode($response->getBody()->getContents())->USD; 
     
-        return dd($res);
-        
-});
-
-
-
-
-//Route::get('btc',[BtcController::class, 'index']);
-    
-
-Route::get('/', function () {
-    return view('welcome');
+    return dd($res); 
 });
